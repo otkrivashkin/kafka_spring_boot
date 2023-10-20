f553468134756d8e725d0a4b3dc7825012124772
Ihor Bondarenko <ibondarenko@codifire.com>
2023-10-20 15:57:07 +0300

    Init

---
+++ src/test/java/com/bin/bonigo/springbootkafka/SpringBootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bin.bonigo.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
